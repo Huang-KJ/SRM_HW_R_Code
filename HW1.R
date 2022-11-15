b5ebd90ccf6368f7bdfc3d220d54509f2402c9cc
Huang-Kyle <w04268@gmail.com>
2022-11-16 01:32:25 +0800

    add commits

--- HW1.R
+++ HW1.R
@@ -1,9 +1,9 @@
-# Import SPSS(SAV format) file #
+## Import SPSS(SAV format) file ##
 library(haven)
-tscs152 <- read_sav("~/Desktop/NCCU/111-1/三1234社會研究方法/HW/tscs152.sav")
+tscs152 <- read_sav(file = "~/Desktop/NCCU/111-1/三1234社會研究方法/HW/tscs152.sav")#check the file path
 View(tscs152)
 
 # Import Stata(DTA format) file #
 library(haven)
-tscs152 <- read_dta("~/Desktop/NCCU/111-1/三1234社會研究方法/HW/tscs152.dta", encoding='big-5')
+tscs152 <- read_dta(file = "~/Desktop/NCCU/111-1/三1234社會研究方法/HW/tscs152.dta", encoding='big-5')#check the file path
 View(tscs152)
