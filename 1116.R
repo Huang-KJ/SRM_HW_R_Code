cecc8ff199b5ba2dca98fea3621ecd8e62a2f5df
Huang-Kyle <w04268@gmail.com>
2022-11-17 23:17:56 +0800

    tab function

--- 1116.R
+++ 1116.R
@@ -1,7 +1,18 @@
 library(haven)
-library(dplyr)
 library(ggplot2)
 library(data.table)
+Tab <- function(data, var1, code, var2) {
+    data <- as.data.frame(data)
+    i <- which(names(data) == var1)
+    j <- which(names(data) == var2)
+    n <- nrow(data[data[, i] == code, ])
+    tableA <- table(data[data[, i] == code, j])
+    Table1 <- cbind(Freq. = tableA,
+                    Percent = round(tableA / n * 100, 3),
+                    Cum. = round(cumsum(tableA) / n * 100,  3)) |> data.frame()
+    Table1 <- rbind(Table1, Total = c(sum(Table1$Freq.), sum(Table1$Percent), ''))
+    return(Table1)
+}
 
 tscs212 <- read_dta("~/Desktop/NCCU/111-1/三1234社會研究方法/HW/tscs212.dta") |> setDT()
 tscs212 <- (tscs212[!(e24c > 5 | e24d > 5 | e24e > 5), ][, ":="(MH = e24c + e24d + e24e)])
@@ -15,68 +26,34 @@ tableB <- cumsum(tableA)
 Table1 <- cbind(Freq. = tableA,
                 Percent = round(tableA / nrow(data1) * 100, 2),
                 Cum. = round(tableB / nrow(data1) * 100, 2)) |> data.frame()
-Table1 <- rbind(Table1, Total = c(sum(Table1$Freq.), sum(Table1$Percent),''))
+Table1 <- rbind(Table1, Total = c(sum(Table1$Freq.), sum(Table1$Percent), ''))
 Table1
 
 # e24c #
-tableA <- table(data1[e24c == 1, MH])
-tableB <- cumsum(tableA)
-Table1 <- cbind(Freq. = tableA,
-                Percent = tableA / nrow(data1[e24c == 1]) * 100,
-                Cum. = tableB / nrow(data1[e24c == 1]) * 100) |> data.frame()
-Table1
+Tab(data1, 'e24c', 1, 'MH')
 ggplot(data1[e24c == 1, .(MH = as.factor(MH))], aes(MH))+
     geom_bar()
 
 # e24d #
-tableA <- table(data1[e24d == 1, MH])
-tableB <- cumsum(tableA)
-Table1 <- cbind(Freq. = tableA,
-                Percent = tableA / nrow(data1[e24d == 1]) * 100,
-                Cum. = tableB / nrow(data1[e24d == 1]) * 100) |> data.frame()
-Table1
+Tab(data1, 'e24d', 1, 'MH')
 ggplot(data1[e24d == 1, .(MH = as.factor(MH))], aes(MH))+
     geom_bar()
 
 # e24e #
-tableA <- table(data1[e24e == 1, MH])
-tableB <- cumsum(tableA)
-Table1 <- cbind(Freq. = tableA,
-                Percent = tableA / nrow(data1[e24e == 1]) * 100,
-                Cum. = tableB / nrow(data1[e24e == 1]) * 100) |> data.frame()
-Table1
+Tab(data1, 'e24e', 1, 'MH')
 ggplot(data1[e24e == 1, .(MH = as.factor(MH))], aes(MH))+
     geom_bar()
 
 # e24f #
-tableA <- table(data1[e24f == 5, MH])
-tableB <- cumsum(tableA)
-Table1 <- cbind(Freq. = tableA,
-                Percent = tableA / nrow(data1[e24f == 5]) * 100,
-                Cum. = tableB / nrow(data1[e24f == 5]) * 100) |> data.frame()
-Table1
+Tab(data1, 'e24f', 5, 'MH')
 ggplot(data1[e24f == 5, .(MH = as.factor(MH))], aes(MH))+
     geom_bar()
 
 # e24g #
-tableA <- table(data1[e24g == 1, MH])
-tableB <- cumsum(tableA)
-Table1 <- cbind(Freq. = tableA,
-                Percent = tableA / nrow(data1[e24g == 1]) * 100,
-                Cum. = tableB / nrow(data1[e24g == 1]) * 100) |> data.frame()
-Table1
+Tab(data1, 'e24g', 1, 'MH')
 ggplot(data1[e24g == 1, .(MH = as.factor(MH))], aes(MH))+
     geom_bar()
 
-
-#----------------------------------------#
-library(gmodels)
-with(data1, CrossTable(e24c))
-Tab <- function(data, var1, code, var2) {
-    table1 <- table(data[data$var1 == code, data$var2]) |> data.frame()
-    table2 <- (cumsum(table1[, 2]) / nrow(data[data$var1 == code,])) |> data.frame()
-    table3 <- data.frame(table1,
-                         table1[, 2] / nrow(data[data$var1 == code, ]), 
-                         table2)
-    names(table3)[1:4] <- c(var2, 'Freq.', 'Percent', 'Cum.')
-}
+#----------------------------#
+#library(gmodels)
+#with(data1, CrossTable(e24c))
